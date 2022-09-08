d3ec5cd8c68403d0ab8274185ddddd42b5c3512a
Sweta Shah <71022313+Sweta8430@users.noreply.github.com>
2022-09-08 22:30:30 +0100

    Added code to the file

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,51 @@
-class Scrabble {
-
-  score() {
-    // Write your implementation here
+const calcScrabbleScore = (letter) => {
+  switch (letter) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+    case "l":
+    case "n":
+    case "r":
+    case "s":
+    case "t":
+      return 1;
+    case "d":
+    case "g":
+      return 2;
+    case "b":
+    case "c":
+    case "m":
+    case "p":
+      return 3;
+    case "f":
+    case "h":
+    case "v":
+    case "w":
+    case "y":
+      return 4;
+    case "k":
+      return 5;
+    case "j":
+    case "x":
+      return 8;
+    case "q":
+    case "z":
+      return 10;
   }
-}
+};
 
-export default Scrabble;
+const score = (string) => {
+  if (string === null || string === "" || string === " \t\n") {
+    // add a conditions as expected
+    return 0;
+  }
+  let score = 0;
+  string = string.toLowerCase(); // converting String in to Lowercase
+  for (let i = 0; i < string.length; i++) {
+    // Generate a loop and add it to the score param
+    score += calcScrabbleScore(string[i]);
+  }
+  return score;
+};
