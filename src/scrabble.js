aa25951087b65deb318bf25f8699e209c77d1897
Christopher West <cwestx99@hotmail.co.uk>
2022-09-12 14:25:45 +0100

    Completed pre-academy-scrabble-challenge
    Using an object along with a for statement

--- src/scrabble.js
+++ src/scrabble.js
@@ -11,35 +11,21 @@ class Scrabble {
     let total = 0;
 
     let points = {
-      1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-      2: ['D', 'G'],
-      3: ['B', 'C', 'M', 'P'],
-      4: ['F', 'H', 'V', 'W', 'Y'],
-      5: ['K'],
-      8: ['J', 'X'],
-      10: ['Q', 'Z'],
+      A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+      D: 2, G: 2,
+      B: 3, C: 3, M: 3, P: 3,
+      F: 4, H: 4, V: 4, W: 4, Y: 4,
+      K: 5,
+      J: 8, X: 8,
+      Q: 10, Z: 10
     };
+
     for (let i = 0; i < this.word.length; i++) {
       const letters = this.word.slice(i);
-      for (let key in points) {
-        if (letters[i] === points[1]) {
-          total += 1
-        } else if (letters[i] === points[2]) {
-          total += 2
-        } else if (letters[i] === points[3]) {
-          total += 3
-        } else if (letters[i] === points[4]) {
-          total += 4
-        } else if (letters[i] === points[5]) {
-          total += 5
-        } else if (letters[i] === points[8]) {
-          total += 8
-        } else if (letters[i] === points[10]) {
-          total += 10
-        }
-      }
-      return total;
+
+      total += points[this.word[i]] || 0;
     }
+    return total
   }
 }
 export default Scrabble;
