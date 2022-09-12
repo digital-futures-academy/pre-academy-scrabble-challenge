4de65f67aa7e2aa29a51346633d082337a450dcb
Christopher West <cwestx99@hotmail.co.uk>
2022-09-12 14:14:55 +0100

    Second attempt using an object

--- src/scrabble.js
+++ src/scrabble.js
@@ -8,65 +8,38 @@ class Scrabble {
   }
 
   score() {
-    let points = 0;
+    let total = 0;
 
-    // Point totals
-    const onePoints = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'];
-    const twoPoints = ['D', 'G'];
-    const threePoints = ['B', 'C', 'M', 'P'];
-    const fourPoints = ['F', 'H', 'V', 'W', 'Y'];
-    const fivePoints = ['K'];
-    const eightPoints = ['J', 'X'];
-    const tenPoints = ['Q', 'Z'];
-
+    let points = {
+      1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+      2: ['D', 'G'],
+      3: ['B', 'C', 'M', 'P'],
+      4: ['F', 'H', 'V', 'W', 'Y'],
+      5: ['K'],
+      8: ['J', 'X'],
+      10: ['Q', 'Z'],
+    };
     for (let i = 0; i < this.word.length; i++) {
       const letters = this.word.slice(i);
-
-      for (let j = 0; j < onePoints.length; j++) {
-        if (letters[i] === onePoints[j]) {
-          points += 1;
+      for (let key in points) {
+        if (letters[i] === points[1]) {
+          total += 1
+        } else if (letters[i] === points[2]) {
+          total += 2
+        } else if (letters[i] === points[3]) {
+          total += 3
+        } else if (letters[i] === points[4]) {
+          total += 4
+        } else if (letters[i] === points[5]) {
+          total += 5
+        } else if (letters[i] === points[8]) {
+          total += 8
+        } else if (letters[i] === points[10]) {
+          total += 10
         }
       }
-
-
-      for (let k = 0; k < twoPoints.length; k++) {
-        if (letters[i] === twoPoints[k]) {
-          points += 2;
-        }
-      }
-
-      for (let l = 0; l < threePoints.length; l++) {
-        if (letters[i] === threePoints[l]) {
-          points += 3;
-        }
-      }
-
-
-      for (let m = 0; m < fourPoints.length; m++) {
-        if (letters[i] === fourPoints[m]) {
-          points += 4;
-        }
-      }
-
-      for (let n = 0; n < fivePoints.length; n++) {
-        if (letters[i] === fivePoints[n]) {
-          points += 5;
-        }
-      }
-
-      for (let o = 0; o < eightPoints.length; o++) {
-        if (letters[i] === eightPoints[o]) {
-          points += 8;
-        }
-      }
-
-      for (let p = 0; p < tenPoints.length; p++) {
-        if (letters[i] === onePoints[p]) {
-          points += 10;
-        }
-      }
+      return total;
     }
-    return points;
   }
 }
 export default Scrabble;
