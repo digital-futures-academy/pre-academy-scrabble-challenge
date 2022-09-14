1ec03db1d5a8ffcb060869b4c4bd6c2fcbb95eb7
Craig Simbarashe Machiya <simba-craig@hotmail.com>
2022-09-14 02:14:13 +0100

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,29 @@
-class Scrabble {
-
-  score() {
-    // Write your implementation here
-  }
+const totalScore = {  //initializing score as a global object variable with key values
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
+} 
+class Scrabble { //class function initiated 
+  constructor(word) {  //properties assigend inside constructor method.
+    if (word == null) { 
+       this.word = '';
+       } else {
+      this.word = word.toUpperCase();
+       }
+    }  //if statement wiill check if the word is null and if not it will capitalize the word
+       //so it can be read by the for loop below acccording to the values decleared in the 
+       //global variable.
+       
+  score() { 
+    sum = 0; //initial score
+    for (let i = 0; i < this.word.length; i++) { //loop will run through each letter/index, 
+      sum += totalScore[this.word[i]] || 0; //add the points from each index if invalid character, score will be 0
+      } console.log(sum);
+        return 0;
+      }
 }
-
-export default Scrabble;
+export default Scrabble; 
