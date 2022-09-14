ddaacde8c92f92303c1c3cb430016884dcf0f091
Molly Stemp <108807597+Molly011@users.noreply.github.com>
2022-09-14 20:20:26 +0100

    Scrabble challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,63 @@
-class Scrabble {
 
-  score() {
-    // Write your implementation here
+    function scrabbleScore(str) {
+    let one = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"];
+    let two = ["d", "g"];
+    let three = ["b", "c", "m", "p"];
+    let four = ["f", "h", "v", "w", "y"];
+    let five = ["k"];
+    let eight = ["j", "x"];
+    let ten = ["q", "z"]
+
+    let score = 0;
+
+    let arr = str.toLowerCase().split('');
+
+    for (let i of arr) {
+        one.forEach(item => {
+            if (i === item) { score += 1 }
+        })
+    }
+
+    for (let i of arr) {
+        two.forEach(item => {
+            if (i === item) { score += 2 }
+        })
+    }
+
+    for (let i of arr) {
+        three.forEach(item => {
+            if (i === item) { score += 3 }
+        })
+    }
+
+    for (let i of arr) {
+        four.forEach(item => {
+            if (i === item) { score += 4 }
+        })
+    }
+
+    for (let i of arr) {
+        five.forEach(item => {
+            if (i === item) { score += 5 }
+        })
+    }
+
+    for (let i of arr) {
+        eight.forEach(item => {
+            if (i === item) { score += 8 }
+        })
+    }
+
+    for (let i of arr) {
+        ten.forEach(item => {
+            if (i === item) { score += 10 }
+        })
+    }
+
+
+    return score
+
+}
   }
 }
 
-export default Scrabble;
