e87bf6fb228bae4354756703235638fdf2501cfc
alexhamma <alexhammad@hotmail.co.uk>
2022-09-14 15:39:51 +0100

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,19 +1,24 @@
 class Scrabble {
+    constructor(word) {
+    this.word = word;
+  }
+  
+  
+  score(Word1) {
+    let newAlphabet = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 };
+    var sum = 0;
+    //i;
+
+    Word1 = Word1.toLowerCase();
+    for (i = 0; i < word.length; i++) {
+      sum += newAlphabet[Word1[i]] || 0; 
+    }
+    return sum;
+  }
   
   
   score(word) {
-    var scores = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10];
-    var score = 0;
-    word = word.toUpperCase();
-    for (var i = 0; i < word.length; i++) {
-      var n = word.charCodeAt(i) - 65;
-      if (n < 0 || n > 25) continue;
-      score += scores[n];
-    }
-    return score;
-  }
-     
-  }
+    
      
     
     // Write your implementation here
