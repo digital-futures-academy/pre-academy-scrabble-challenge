6fb6fde15862fe67fb2e18cc85c25f26fce1aa3e
alexhamma <alexhammad@hotmail.co.uk>
2022-09-14 14:21:54 +0100

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,17 @@
 class Scrabble {
   
   
-  score() {
+  score(word) {
+    var scores = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10];
+    var score = 0;
+    word = word.toUpperCase();
+    for (var i = 0; i < word.length; i++) {
+      var n = word.charCodeAt(i) - 65;
+      if (n < 0 || n > 25) continue;
+      score += scores[n];
+    }
+    return score;
+  }
      
   }
      
