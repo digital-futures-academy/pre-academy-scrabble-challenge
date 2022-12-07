ec2c31e11a8623d090d4b6b5cb90bc64363caa65
Krisztian <krisztian.valyi1@gmail.com>
2022-12-07 11:17:29 +0000

    created scrabble challenge for pre-academy work

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,69 @@
 class Scrabble {
-
+  constructor(word) {
+    this._word = word;
+  }
+  get word() {
+    return this._word;
+  }
+  set word(word) {
+    return (this._word = word);
+  }
   score() {
-    // Write your implementation here
+    let score = 0;
+    if (this.word === null) {
+      return 0;
+    } else {
+      const trimmedWord = this.word.trim().toUpperCase();
+      if (trimmedWord === "") {
+        return score;
+      } else {
+        for (let i = 0; i < trimmedWord.length; i++) {
+          switch (trimmedWord[i]) {
+            case "A":
+            case "E":
+            case "I":
+            case "O":
+            case "U":
+            case "L":
+            case "N":
+            case "R":
+            case "S":
+            case "T":
+              score += 1;
+              break;
+            case "D":
+            case "G":
+              score += 2;
+              break;
+            case "B":
+            case "C":
+            case "M":
+            case "P":
+              score += 3;
+              break;
+            case "F":
+            case "H":
+            case "V":
+            case "W":
+            case "Y":
+              score += 4;
+              break;
+            case "K":
+              score += 5;
+              break;
+            case "J":
+            case "X":
+              score += 8;
+              break;
+            case "Q":
+            case "Z":
+              score += 10;
+              break;
+          }
+        }
+        return score;
+      }
+    }
   }
 }
 
