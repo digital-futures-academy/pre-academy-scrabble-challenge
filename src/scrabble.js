453765a9eeb46cae36ad3c26ab0e5e6926fab925
Ashleigh Morton <ashleigh5@hotmail.co.uk>
2022-12-11 14:39:09 +0000

    first commit

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,25 @@
 class Scrabble {
 
+  constructor(inputWord) {
+    this.scrabbleWord = inputWord;
+  }
+  total = 0;
+
+  pointsMap = new Map([
+    ['a', 1], ['b', 3], ['c', 3], ['d', 2], ['e', 1], ['f', 4], ['g', 2], ['h', 4], ['i', 1], ['j', 8], ['k', 5], ['l', 1], ['m', 3], ['n', 1], ['o', 1], ['p', 3], ['q', 10], ['r', 1], ['s', 1], ['t', 1], ['u', 1], ['v', 4], ['w', 4], ['x', 8], ['y', 4], ['z', 10]
+  ]);
+
   score() {
-    // Write your implementation here
+    if (this.scrabbleWord === null) return 0;
+    this.calculateScore();
+    return this.total;
+  }
+
+  calculateScore() {
+    let array = this.scrabbleWord.toLowerCase().trim().split(''); // ['s','m','u','d','g','e']
+    for (let i in array) {
+      this.total = this.total + this.pointsMap.get(array[i]);
+    }
   }
 }
 
