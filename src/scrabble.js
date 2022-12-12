507c860579076a03dded1300d7325c1ade058a3d
Pierre Raphael <pierreraphael96@gmail.com>
2022-12-12 18:52:08 +0000

    Updated Scrabble Code

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,73 @@
 class Scrabble {
 
-  score() {
-    // Write your implementation here
+  // starting values
+  scoreSum = 0;
+  word = "";
+
+  //change the word
+  constructor(inputWord) {
+    if (inputWord === null || inputWord === "") {
+      return this.scoreSum;
+    } else {
+      this.word = inputWord;
+    }
+  }
+
+
+  // work out total points
+  totalPoints() {
+    // lowercase and changestring  into array
+    const updatedWord = this.word.toLowerCase().split("");
+
+    //loop for points per letter then add to total points
+    updatedWord.forEach((letter) => {
+      switch (letter) {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+        case "l":
+        case "n":
+        case "r":
+        case "s":
+        case "t":
+          this.scoreSum += 1;
+          break;
+        case "g":
+        case "d":
+          this.scoreSum += 2;
+          break;
+        case "b":
+        case "m":
+        case "p":
+        case "c":
+          this.scoreSum += 3;
+          break;
+        case "y":
+        case "w":
+        case "v":
+        case "f":
+        case "h":
+          this.scoreSum += 4;
+          break;
+        case "k":
+          this.scoreSum += 5;
+          break;
+        case "x":
+        case "j":
+          this.scoreSum += 8;
+          break;
+        case "z":
+        case "q":
+          this.scoreSum += 10;
+          break;
+      }
+    });
+    return this.scoreSum;
+  }
+
+  totalScore(){
+   return this.totalPoints();
   }
 }
-
-export default Scrabble;
