775d44988c9c529fa565ddee5d572970a63d5ef6
Matt Jones <119697940+mattjones26987@users.noreply.github.com>
2022-12-17 15:18:12 +0000

    First attempe

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,23 @@
 class Scrabble {
 
   score() {
-    // Write your implementation here
+    if (score === ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]) {
+      return (1)
+    } else if (score === ["D", "G"]) {
+      return (2)
+    } else if (score === ["B", "C", "M", "P"]) {
+      return (3)
+    } else if (score === ["F", "H", "V", "W", "Y"]) {
+      return (4)
+    } else if (score === ["k"]) {
+      return (5)
+    } else if (score === ["J", "X"]) {
+      return (8)
+    } else if (score === ["Q", "Z"]) {
+      return (10)
+    } else {
+      return (0)
+    }
   }
 }
 
