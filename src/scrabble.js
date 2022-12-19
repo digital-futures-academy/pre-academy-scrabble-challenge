f203de79df8de34352b7a44991c5ea0b1dad8469
celinealum <alumceline@gmail.com>
2022-12-19 02:32:11 +0000

    g

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,62 @@
 class Scrabble {
 
+  constructor(word) {
+    if (word === null) {
+      this.word = "";
+    } else {
+      this.word = word.toUpperCase();
+    }
+  }
+
   score() {
     // Write your implementation here
+
+    //set pointValue
+
+    let pointValue = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 5,
+      Z: 10,
+    };
+
+    //initial score
+
+    let result = 0
+
+    for (let i = 0; i <= this.word.length; i++) {
+
+      const newarray = this.word.slice(i);
+      result += pointValue[this.word[i].toUpperCase()] || 0;
+    }
+    return result;
+
+
+
+
+
   }
 }
 
