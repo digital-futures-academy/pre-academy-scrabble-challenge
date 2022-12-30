572064d58a9e0c19ee016fcb1cdd9d0b1613c4dd
Francisco Bejarano <bejarano.fco@gmail.com>
2022-12-30 22:56:41 +0000

    Solution for Scrabble Challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,70 @@
 class Scrabble {
+  
+  score(word) {
+    const lettersValue = {
+      A: 1,
+      E: 1,
+      I: 1,
+      O: 1,
+      U: 1,
+      L: 1,
+      N: 1,
+      R: 1,
+      S: 1,
+      T: 1,
+      D: 2,
+      G: 2,
+      B: 3,
+      C: 3,
+      M: 3,
+      P: 3,
+      F: 4,
+      H: 4,
+      V: 4,
+      W: 4,
+      Y: 4,
+      K: 5,
+      J: 8,
+      X: 8,
+      Q: 10,
+      Z: 10,
+    };
+    
+    let totalPoints = 0;
 
-  score() {
-    // Write your implementation here
+    if(!word || word.includes(" ")){
+      return totalPoints;
+    }
+    else {
+      word = word.toUpperCase();
+
+      for(var i = 0; i < word.length; i++){
+  
+        totalPoints += lettersValue[word[i]];
+      }
+      
+      return totalPoints;
+    }
+    
+
   }
 }
 
 export default Scrabble;
+
+let scrabble = new Scrabble();
+console.log(scrabble.score('')); // => 0
+
+console.log(scrabble.score(" \t\n")) // => 0
+
+console.log(scrabble.score(null)) // => 0
+
+console.log(scrabble.score('a')) // => 1
+
+console.log(scrabble.score('f')) // => 4
+
+console.log(scrabble.score('street')) // => 6
+
+console.log(scrabble.score('quirky')) // => 22
+
+console.log(scrabble.score('OXYPHENBUTAZONE')) // => 41
