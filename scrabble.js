19888d3999f09c06c956f3456d937b13fa46dd05
Matt Jones <119697940+mattjones26987@users.noreply.github.com>
2023-01-02 14:24:56 +0000

    New Try

--- scrabble.js
+++ scrabble.js
@@ -1,51 +1,46 @@
-/*class Scrabble {
+/*class Scrabble { }
 
-  score() {
-    if (score === ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]) {
-      return 1
-    } else if (score === ["D", "G"]) {
-      return 2
-    } else if (score === ["B", "C", "M", "P"]) {
-      return 3
-    } else if (score === ["F", "H", "V", "W", "Y"]) {
-      return 4
-    } else if (score === ["k"]) {
-      return 5
-    } else if (score === ["J", "X"]) {
-      return 8
-    } else if (score === ["Q", "Z"]) {
-      return 10
-    } else {
-      return 0
-    }
-  }
+if (score === ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]) {
+  return 1
+} else if (score === ["D", "G"]) {
+  return 2
+} else if (score === ["B", "C", "M", "P"]) {
+  return 3
+} else if (score === ["F", "H", "V", "W", "Y"]) {
+  return 4
+} else if (score === ["k"]) {
+  return 5
+} else if (score === ["J", "X"]) {
+  return 8
+} else if (score === ["Q", "Z"]) {
+  return 10
+} else {
+  return 0
 }
+
+
+
 
 export default Scrabble;*/
 
+class Scrabble {
+  score() {
 
+    function list(letter) {
+      Array = letter.toUpperCase().split('')
+      score = 0
 
-function scrabble(score) {
-  score = score.toLowerCase();
-  var count = 0;
-  for (var i = 0; i < score.length; ++i) {
-    if (score[i] == 'a' || score[i] == 'e' || score[i] == 'i' || score[i] == 'o' || score[i] == 'u' || score[i] == 'l' || score[i] == 'n' || score[i] == 'r' || score[i] == 's' || score[i] == 't')
-      count++;
-    if (score[i] == 'd' || score[i] == 'g')
-      count += 2;
-    if (score[i] == 'b' || score[i] == 'c' || score[i] == 'm' || score[i] == 'p')
-      count += 3;
-    if (score[i] == 'f' || score[i] == 'h' || score[i] == 'v' || score[i] == 'w' || score[i] == 'y')
-      count += 4;
-    if (score[i] == 'k')
-      count += 5;
-    if (score[i] == 'j' || score[i] == 'x')
-      count += 8;
-    if (score[i] == 'q' || score[i] == 'z')
-      count += 10;
+      var dict = {
+        " ": 0,
+        "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
+        "D": 2, "G": 2,
+        "B": 3, "C": 3, "M": 3, "P": 3,
+        "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+        "K": 5,
+        "J": 8, "X": 8,
+        "Q": 10, "Z": 10
+      }
+    }
   }
-
-  return count;
 }
-
-export default scrabble.score;
+export default Scrabble;
