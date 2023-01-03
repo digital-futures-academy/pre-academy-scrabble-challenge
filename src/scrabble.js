f293c2b6345b88e92901bb97cd9a6dd11e4a77b2
mimimingfei <121249959+mimimingfei@users.noreply.github.com>
2023-01-03 16:02:17 +0000

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,38 @@
 class Scrabble {
-
-  score() {
-    // Write your implementation here
+  constuctor(word) {
+    this.word = word;
   }
+  
+ const score = (word) => {
+  let points = 0;
+   if this.word == '' || this.word == null) {
+     return points = 0
+   } else {
+  for (let i = 0; i < this.word.length; i++) {
+    if (word[i].toUpperCase() === 'A' || 'E' || 'I' ||'O' || 'U' || 'L' || 'N' || 'R' || 'S' || 'T') {
+      points ++;
+    } else if (word[i].toUpperCase() === 'D' || 'G') {
+      points += 2;
+     } else if (word[i].toUpperCase() === 'B' || 'C' || 'M' || 'P') {
+        points += 3;
+     } else if (word[i].toUpperCase() === 'F' || 'H' || 'V' || 'W' || 'Y') {
+       points += 4;
+      }  else if (word[i].toUpperCase() === 'K') {
+          points += 5;
+        } else if (word[i].toUpperCase() === 'J' || 'X') {
+          points += 8;
+        } else if (word[i].toUpperCase() === 'Q' || 'Z') {
+          points += 10 ;
+        } else {
+          points += 0;
+        }
 }
+   }
+return points
+
+
+
+
+
 
 export default Scrabble;
