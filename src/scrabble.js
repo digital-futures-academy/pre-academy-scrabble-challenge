b4bfde626cbb09a502dc936ad901501eee3741c9
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-04-25 13:07:31 +0100

    Tested coded. Added comments for clarity.

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,14 +1,16 @@
 class Scrabble {
-
+//Constructor takes parameter word and sets it to uppercase string. 
+//If parameter not passed, empty string is assigned.
   constructor(word) {
     this.word = (word || '').toUpperCase();
   }
-
+//Score method calculates the score of the 'word'. scoreSum is set to zero.
+// For loop iterates over each letter of word.
   score() {
    let scoreSum = 0;
 
    for (let i = 0; i < this.word.length; i++) {
-    
+//Switch statement calculates score for the letter and adds it to scoreSum.
     const letter = this.word[i];
     switch(letter) {
       case 'A':
@@ -56,26 +58,36 @@ class Scrabble {
 
     }
    }
+   //scoreSum contains total score of the word.
    return scoreSum;
   }
 }
-let scrabble = new Scrabble('street');
+//Testing code
+let scrabble = new Scrabble('quirky');
 console.log(scrabble.score()); 
 
-//Extended Criteria Attempt
+
+//Extended Criteria Attempt(1st piece of the Extended Criteria)
+
+//Attempt at extended criteria with slightly different class: Scrabble_new
+
 class Scrabble_new {
+  
   constructor(word) {
     this.word = (word || '').toUpperCase();
   }
+  //Score method calculates the score of the word according to letter values in variable letterPoints.
   score(){
     const letterPoints = {A:1, E:1, I:1, O:1, U:1, L:1, N:1, R:1, S:1, T:1, D:2, G:2, B:3,C:3,M:3,P:3,F:4,H:4,V:4,W:4,Y:4,K:5,J:8,X:8,Q:10,Z:10};
     let scoreSum = 0;
+  //For...of loop to loop through each character in this.word.
     for(const letter of this.word) {
       scoreSum += letterPoints[letter] || 0;
     }
     return scoreSum;
   }
 }
+//Testing code
 let scrabble1 = new Scrabble_new('street');
 console.log(scrabble1.score());
 
