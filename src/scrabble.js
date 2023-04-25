d8ab8c0e29756a3b05d916fe4fb817f91a9ad5ab
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-04-25 12:27:30 +0100

    Attempted Extended Criteria(1) and tested code. This can be found under comment: Extended Criteria Attempt.

--- src/scrabble.js
+++ src/scrabble.js
@@ -62,4 +62,21 @@ class Scrabble {
 let scrabble = new Scrabble('street');
 console.log(scrabble.score()); 
 
+//Extended Criteria Attempt
+class Scrabble_new {
+  constructor(word) {
+    this.word = (word || '').toUpperCase();
+  }
+  score(){
+    const letterPoints = {A:1, E:1, I:1, O:1, U:1, L:1, N:1, R:1, S:1, T:1, D:2, G:2, B:3,C:3,M:3,P:3,F:4,H:4,V:4,W:4,Y:4,K:5,J:8,X:8,Q:10,Z:10};
+    let scoreSum = 0;
+    for(const letter of this.word) {
+      scoreSum += letterPoints[letter] || 0;
+    }
+    return scoreSum;
+  }
+}
+let scrabble1 = new Scrabble_new('street');
+console.log(scrabble1.score());
+
 export default Scrabble;
