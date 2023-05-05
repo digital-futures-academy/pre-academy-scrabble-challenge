ecae6ba2beb83d7d621d3be801cbba9c9bb5a9fd
Xavier Bugg <xtwb2@cam.ac.uk>
2023-05-05 12:07:07 +0100

    Inital completed scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,43 @@
+const scrabbleScores = new Map([["a", 1],
+["b", 3],
+["c", 3],
+["d", 2],
+["e", 1],
+["f", 4],
+["g", 2],
+["h", 4],
+["i", 1],
+["j", 8],
+["k", 5],
+["l", 1],
+["m", 3],
+["n", 1],
+["o", 1],
+["p", 3],
+["q", 10],
+["r", 1],
+["s", 1],
+["t", 1],
+["u", 1],
+["v", 4],
+["w", 4],
+["x", 8],
+["y", 4],
+["z", 10]
+]);
 class Scrabble {
+  constructor(word) {
+    this.word = (typeof word === "string" ? word.toLowerCase().trim() : "");
+  }
 
   score() {
-    // Write your implementation here
+    var score = 0;
+    for (const letter of this.word) {
+      if (!scrabbleScores.has(letter)) return 0;
+      score += scrabbleScores.get(letter);
+    }
+    return score;
+
   }
 }
-
 export default Scrabble;
