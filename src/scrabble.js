8eaab1931331da1eabeff142b63a48dc6c0afbe5
kenichi beveridge <kenbev84@gmail.com>
2023-05-05 12:13:01 +0100

    Added constructor to Scrabble class and loadData function to read from wordScore.txt.

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,27 @@
-class Scrabble {
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+class Scrabble {
+  constructor(word) {
+    this._word = word;
+  }
+
+  loadData() {
+    const fileData = fs.readFileSync(path.join(__dirname, './wordScore.txt'));
+    const stringData = fileData.toString();
+    return stringData;
+  }
   score() {
     // Write your implementation here
   }
 }
 
 export default Scrabble;
+
+// const testScrabble = new Scrabble('hello');
+
+// console.log(testScrabble.loadData());
