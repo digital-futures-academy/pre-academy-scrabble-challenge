9f92305fd714184944e48aea1138ed31043e4c04
Liam Risdon <131891513+liamrisdon@users.noreply.github.com>
2023-05-09 11:32:42 +0100

    Minimum Acceptance Criteria

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,27 @@
 class Scrabble {
+  constructor(word) {
+    this.word = word.toUpperCase();
+    this.letterValueTable = {
+      'AEIOULNRST': 1,
+      'DG': 2,
+      'BCMP': 3,
+      'FHVWY': 4,
+      'K': 5,
+      'JX': 8,
+      'QZ': 10
+    }
+  }
 
   score() {
-    // Write your implementation here
+    let totalScore = 0;
+    for (let i = 0; i < this.word.length; i++) {
+      for (let letters in this.letterValueTable) {
+        if (letters.includes(this.word[i])) {
+          totalScore += this.letterValueTable[letters];
+          break;
+        }
+      }
+    }
   }
 }
 
