2dbfc9871a97fb1c8cceb5a5bd56f4729900fc3f
Daniel Ayesoro <dayesoro@gmail.com>
2023-05-09 21:47:10 +0100

    solved the challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,34 @@
 class Scrabble {
 
-  score() {
-    // Write your implementation here
+  _letterData = { 1: "AEIOULNRST", 2: "DG", 3: "BCMP", 4: "FHVWY", 5: "K", 8: "JX", 10: "QZ" }
+  _score = 0;
+  _words = '';
+
+
+  constructor(words) {
+
+    if (words) {
+      this._words = words.toUpperCase();
+    }
   }
+
+  score() {
+    for (let letter of this._words) {
+      this._checkLetterPoint(letter)
+    }
+    return this._score;
+
+  }
+
+  _checkLetterPoint(letter) {
+    for (let value in this._letterData) {
+      if (this._letterData[value].includes(letter)) {
+        this._score = this._score + parseInt(value);
+      }
+    }
+  }
+
 }
+
 
 export default Scrabble;
