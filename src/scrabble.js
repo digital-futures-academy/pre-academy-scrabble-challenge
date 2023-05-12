cb992f3a8376d7da96ade6ccb2e71b6d0f2c9c16
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2023-05-12 14:17:51 +0100

    bonus scores with blank letters

--- src/scrabble.js
+++ src/scrabble.js
@@ -7,7 +7,7 @@ class Scrabble {
     "K": 5,
     "JX": 8,
     "QZ": 10
-  };
+  }; // could add " ": 0 for blanks or could use bonus_multiplier of 0 for those letters.
   score_table = Object.entries(this.letter_values).reduce((lookup_table, [letters, score]) => {
     for (const letter of letters) { lookup_table[letter] = score } // used to be Array.from(letters).forEach(letter => lookup_table[letter] = score);
     return lookup_table;
@@ -26,7 +26,15 @@ class Scrabble {
   }
 
   score(bonus_letter_index_multipliers = {}, total_word_score_multiplier = 1) {
-    return total_word_score_multiplier * Array.from(this._word, (letter, letter_index) => this.score_table[letter] * (bonus_letter_index_multipliers[letter_index] || 1)).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return total_word_score_multiplier * Array.from(this._word,
+      (letter, letter_index) =>
+        this.score_table[letter] * this.get_letter_index_multiplier(letter_index, bonus_letter_index_multipliers))
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+
+  get_letter_index_multiplier(letter_index, bonus_letter_index_mulitpliers = {}) {
+    let possible_letter_multiplier = bonus_letter_index_mulitpliers[letter_index];
+    return possible_letter_multiplier === undefined ? 1 : possible_letter_multiplier;
   }
 }
 
@@ -63,3 +71,4 @@ console.log(scrabble.score()); // 15
 console.log(scrabble.score({}, 2)); // double word -> 30
 console.log(scrabble.score({ 0: 2 })); // double letter on first letter -> 25
 console.log(scrabble.score({ 0: 2, 4: 3 }, 2)); // double letter on first letter, triple on fifth letter, double word -> 54
+console.log(scrabble.score({ 0: 0, 1: 0 }, 3)); // two blanks (not sure this is possible in scrabble but is fine for testing) and a triple word -> 12
