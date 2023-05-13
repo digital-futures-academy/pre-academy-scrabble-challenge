d6fb061716c75d56be811570ebd21bac10d0235b
austinventour <austin_ventour@hotmail.co.uk>
2023-05-13 15:38:13 +0100

    Initial Commit

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,24 @@
 class Scrabble {
 
   score() {
-    // Write your implementation here
+    const letterValues = {
+      A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4,
+      I: 1, J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3,
+      Q: 10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8,
+      Y: 4, Z: 10
+    };
+
+
+
   }
 }
 
+
+
+
 export default Scrabble;
+<Question ID="1" Shortcut="Q1" Order="" ElementType="question" QuestionType="numeric" Anonymity="1" AllowDK="1" Decimals="0" Translated="0" >
+  <LongCaption></LongCaption>
+  <Routings>
+  </Routings>
+</Question>
