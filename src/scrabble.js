a1f41ce2aefa10ef1b44ac885173dc746b1b1d59
Marta Bianchini <marta.bianchini24@gmail.com>
2023-05-16 14:16:29 +0100

    Implemented dictonary score object and logic for words with all letters

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,34 @@
 class Scrabble {
   constructor(word) {
     this.word = word;
+    this.points = {
+      'A': 1,
+      'B': 3,
+      'C': 3,
+      'D': 2,
+      'E': 1,
+      'F': 4,
+      'G': 2,
+      'H': 4,
+      'I': 1,
+      'J': 8,
+      'K': 5,
+      'L': 1,
+      'M': 3,
+      'N': 1,
+      'O': 1,
+      'P': 3,
+      'Q': 10,
+      'R': 1,
+      'S': 1,
+      'T': 1,
+      'U': 1,
+      'V': 4,
+      'W': 4,
+      'X': 8,
+      'Y': 4,
+      'Z': 10,
+    }
   }
 
   score() {
@@ -9,6 +37,12 @@ class Scrabble {
     if (this.word === '' || this.word === null) {
       return score
     }
+
+    const letters = this.word.split('')
+    letters.forEach(letter => {
+      score += this.points[letter.toUpperCase()]
+    });
+    return score
   }
 }
 
