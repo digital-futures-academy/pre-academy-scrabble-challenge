447263e6b616d5de707b6a23dc1bb262887258c0
Dileep <dileepvunnam@gmail.com>
2023-05-17 21:21:27 +0100

    Checking then input

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,13 @@
 class Scrabble {
+constructor(word) {
+    this.word = word; // Store the word passed to the constructor as a property of the instance
+  }
 
   score() {
-    // Write your implementation here
-  }
+    if (!this.word || typeof this.word !== 'string') {
+      return 0; // If the word is falsy or not a string, return 0 as the score
+    }
+  
 }
 
 export default Scrabble;
