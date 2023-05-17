c55e2f862f8ab7c7ada89ed480a9c635a06e0ee0
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-05-17 19:50:57 +0300

    Added letter groupings to the implementation

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,20 @@
 class Scrabble {
 
-  score() {
+  score(word) {
     // Write your implementation here:
+
+    //Partition a variable for the running sum of points
+    let runningsum = 0
+    //Group the points by letters so that the conditional statements below are clear and consise
+    let lettergroup1 = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"];
+    let lettergroup2 = ["d", "g"];
+    let lettergroup3 = ["b", "c", "m", "p"];
+    let lettergroup4 = ["f", "h", "v", "w", "y"];
+    let lettergroup5 = ["k"];
+    let lettergroup6 = ["j", "x"];
+    let lettergroup7 = ["q", "z"];
+
+
   }
 }
 
