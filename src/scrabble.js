a154ac161f97d45e67b304c7e7241d8457f2c4f9
Chioma Onyeneke <ccchioma60@gmail.com>
2023-05-17 14:30:19 +0100

    modify solution to scrabble challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,24 +1,39 @@
 class Scrabble {
+
   constructor(word) {
-    this.word = word
-  };
-  score() {
-    // Write your implementation here
-    const scrabbleDict = {
-      "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I":1, "J":8,
-      "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1,
-      "V":4, "W":4, "X":8, "Y":4, "Z":10
+    if (this.isValidWord(word)){
+      this.word = word.toUpperCase();
     }
-    let counter = 0;
-    if (typeof this.word != "string" || this.word.trim() == "" ) {
-      return counter;
+  }
+
+
+  isValidWord(string) {
+    if (typeof string != "string" || string.trim() == "") {
+      return false;
     }
-    for (let i = 0; i < this.word.length; i++){
-      counter += scrabbleDict[this.word[i].toUpperCase()];
+    return true;
+  }
+
+   score() {
+    if (typeof this.word !== "undefined") {
+      return this.getWordScore(this.word);
+    }
+    return 0;
+  }
+
+  getWordScore(word) {
+     let counter = 0
+    for (let i = 0; i < word.length; i++){
+      counter += this.scrabble(word[i])
     }
     return counter;
   }
+  scrabble(char) {
+    const scrabbleDict = { "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10 };
+    return scrabbleDict[char];
+  }
 }
+
 let scrabble1 = new Scrabble('')
 console.log(scrabble1.score()) // => 0
 
