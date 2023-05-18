e35be976f2c68c0c9cde8a802dff4ca559bae56c
Habeeb Grillo <habeebgrillo@gmail.com>
2023-05-18 18:07:26 +0100

    Solved the Scrabble Challenge

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,26 @@
 class Scrabble {
 
+  totalScore = 0;
+
+
+  letterValues = { 'AEIOULNRST': 1, 'DG': 2, 'BCMP': 3, 'FHVWY': 4, 'K': 5, 'JX': 8, 'QZ': 10 };
+
+  _word = '';
+  constructor(word) {
+    this._word = word ? word.toUpperCase() : '';
+  }
   score() {
-    // Write your implementation here
+
+    for (let letter of this._word) {
+      for (const letters in this.letterValues) {
+        if (letters.includes(letter)) {
+          const letterScore = this.letterValues[letters];
+          this.totalScore += letterScore;
+          break;
+        }
+      }
+    }
+    return this.totalScore;
   }
 }
 
