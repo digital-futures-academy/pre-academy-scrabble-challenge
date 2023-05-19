5455fb5058f1a54da972e6f049f93a3faaacc5b7
Tha Pyay Hmu <104383728+thapyayhmuTP@users.noreply.github.com>
2023-05-19 13:07:47 +0100

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,7 +1,28 @@
 class Scrabble {
 
   score() {
-    // Write your implementation here
+    var letters = { 
+      1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], 
+      2: ['D', 'G'], 
+      3: ['B', 'C', 'M', 'P'],  
+      4: ['F', 'H', 'V', 'W', 'Y'], 
+      5: ['K'],   
+      8: ['J', 'X'], 
+      10: ['Q', 'Z'] };
+    let sum=0;
+    function lettersToScore(letter) {
+      for (var i in letters)
+      {
+        if (Object.keys(letters).indexOf((letters[index].indexOf(letter) !== -1) ? index.toString() : '-1') !== -1) 
+           return parseInt(index);
+      }}
+    var score = function(input) {
+       if (input === null || input === "")
+         return sum;
+      else
+        input.toUpperCase().split("").forEach(function(elem, index, array) 
+        sum += lettersToScore(elem);
+        return sum;
   }
 }
 
