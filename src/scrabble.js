e902ee4988dccfa6f20f4820d0ce87fcb6b24a00
Tha Pyay Hmu <104383728+thapyayhmuTP@users.noreply.github.com>
2023-05-19 13:41:36 +0100

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,23 +1,30 @@
 class Scrabble {
-  constructor(word)
-  {
-    if( word== null)
-      this.word='';
-    else
-      this.word=word.toLowerCase();
-  }
   score() {
-    const letters = { a:1, b:3, c:3, d:2, e:1, f:4, g:2, 
-                         h:4, i:1, j:8, k:5, l:1, m:3, n:1, 
-                         o:1, p:3, q:10, r:1, s:1, t:1, u:1, 
-                         v:4, w:4, x:8, y:4, z:10 };
-    let sum=0;
-    for(let i=0;i<this.word.length;i++)
-    {
-      const scores=this.word.slice(i);
-      sum+=letters[this.word[i]] || 0;
-    }
-    return sum;
+    var letters = { 
+      1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], 
+      2: ['D', 'G'], 
+      3: ['B', 'C', 'M', 'P'],  
+      4: ['F', 'H', 'V', 'W', 'Y'], 
+      5: ['K'],   
+      8: ['J', 'X'], 
+      10: ['Q', 'Z'] };
+
+    function lettersToScore(letter) {
+      for (var i in letters)
+      {
+        if (Object.keys(letters).indexOf((letters[i].indexOf(letter) !== -1) ? i.toString() : '-1') !== -1) 
+           return parseInt(i);
+      }}
+    var score = function(input) {
+       if (input === null || input === "")
+         return sum;
+      else{
+        input.toUpperCase().split("").forEach(function(elem, index, array) 
+        sum += lettersToScore(elem);
+        return sum;
+      }
+  }
+
   }
 }
 
