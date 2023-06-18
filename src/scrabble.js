8e83642e8c8445fee28972764e875be14baac0a9
James <jamesebrahem@hotmail.com>
2023-06-18 20:15:06 +0100

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,53 @@
 class Scrabble {
-
-  score() {
+  score(word) {
     // Write your implementation here
+    const letterValues = {
+      'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+      'D': 2, 'G': 2,
+      'B': 3, 'C': 3, 'M': 3, 'P': 3,
+      'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+      'K': 5,
+      'J': 8, 'X': 8,
+      'Q': 10, 'Z': 10
+    };
+
+    let score = 0;
+    if (word) {
+      for (let i = 0; i < word.length; i++) {
+        const letter = word[i].toUpperCase();
+        const letterScore = letterValues[letter];
+        score += letterScore ? letterScore : 0;
+      }
+    }
+
+    return score;
   }
 }
+
+
+let scrabble1 = new Scrabble();
+console.log(scrabble1.score(''));          // => 0
+
+let scrabble2 = new Scrabble();
+console.log(scrabble2.score(' \t\n'));     // => 0
+
+let scrabble3 = new Scrabble();
+console.log(scrabble3.score(null));        // => 0
+
+let scrabble4 = new Scrabble();
+console.log(scrabble4.score('a'));         // => 1
+
+let scrabble5 = new Scrabble();
+console.log(scrabble5.score('f'));         // => 4
+
+let scrabble6 = new Scrabble();
+console.log(scrabble6.score('street'));    // => 6
+
+let scrabble7 = new Scrabble();
+console.log(scrabble7.score('quirky'));    // => 22
+
+let scrabble8 = new Scrabble();
+console.log(scrabble8.score('OXYPHENBUTAZONE'));    // => 41
+
 
 export default Scrabble;
