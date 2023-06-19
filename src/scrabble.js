0317095e2e0da8ba685303a07ad6cd6b096ea2bf
James <jamesebrahem@hotmail.com>
2023-06-19 20:11:22 +0100

    Update scrabble.js

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,5 +1,9 @@
 class Scrabble {
-  score(word) {
+  constructor(word) {
+    this.word = word;
+  }
+
+  score() {
     // Write your implementation here
     const letterValues = {
       'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
@@ -12,9 +16,9 @@ class Scrabble {
     };
 
     let score = 0;
-    if (word) {
-      for (let i = 0; i < word.length; i++) {
-        const letter = word[i].toUpperCase();
+    if (this.word) {
+      for (let i = 0; i < this.word.length; i++) {
+        const letter = this.word[i].toUpperCase();
         const letterScore = letterValues[letter];
         score += letterScore ? letterScore : 0;
       }
@@ -23,31 +27,5 @@ class Scrabble {
     return score;
   }
 }
-
-
-let scrabble1 = new Scrabble();
-scrabble1.score('');          // => 0
-
-let scrabble2 = new Scrabble();
-scrabble2.score(' \t\n');     // => 0
-
-let scrabble3 = new Scrabble();
-scrabble3.score(null);        // => 0
-
-let scrabble4 = new Scrabble();
-scrabble4.score('a');         // => 1
-
-let scrabble5 = new Scrabble();
-scrabble5.score('f');         // => 4
-
-let scrabble6 = new Scrabble();
-scrabble6.score('street');    // => 6
-
-let scrabble7 = new Scrabble();
-scrabble7.score('quirky');    // => 22
-
-let scrabble8 = new Scrabble();
-scrabble8.score('OXYPHENBUTAZONE');    // => 41
-
 
 export default Scrabble;
