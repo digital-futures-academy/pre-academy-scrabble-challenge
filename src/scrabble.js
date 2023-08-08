066e8d6319a01cbb091682dc698a2b7f4a529b65
Ed Wright <ewright@digitalfutures.com>
2023-08-08 10:35:24 +0100

    Solution added

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,8 +1,38 @@
 class Scrabble {
-
-  score() {
-    // Write your implementation here now!
-  }
+    
+    static letterScores = {
+        aeioulnrst: 1,
+        dg: 2,
+        bcmp: 3,
+        fhvwy: 4,
+        k: 5,
+        jx: 8,
+        qz: 10,
+    }
+    
+    constructor(word) {
+        this.word = word?.trim().toLowerCase();
+    }
+    
+    score() {
+        let wordScore = 0;
+        if (!this.word) return wordScore;
+        wordScore += this.scoreWordOnly();
+        return wordScore;
+    }
+    
+    scoreWordOnly() {
+        let score = 0;
+        this.word.split("").forEach(letter => {
+            for(const [key, value] of Object.entries(Scrabble.letterScores)) {
+                if(key.includes(letter)) {
+                    score += value;
+                    break;
+                }
+            }
+        });
+        return score;
+    }
 }
 
 export default Scrabble;
